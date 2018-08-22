51a5bcdce57d77c64d284d8f0089d5c5d14fdd82
Peter-Li1233 <minxiangli08@gmail.com>
2018-08-22 14:36:47 -0400

    change the connection.js file to use Heroku database

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
 
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,6 +12,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
